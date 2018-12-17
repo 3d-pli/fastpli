fe1ea41c2bea76d1a4b01afa1e2a86765513b7aa
fmatuschke <f.matuschke@fz-juelich.de>
2018-12-17 18:22:23 +0100

    ADD example model.solver, example requirements

---
+++ example/model_solver.py
@@ -0,0 +1,36 @@
+import numpy as np
+import fastpli
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+fiber_bundles = [[
+    fastpli.objects.Fiber([-1, -1, -1, 1, 1, 1], [1, 1]),
+    fastpli.objects.Fiber([1, 1, -1, -1, -1, 1], [1, 1])]]
+
+solver = fastpli.model.Solver()
+
+solver.set_fibers(fiber_bundles)
+solver.set_parameter(drag=0, obj_min_radius=10, obj_mean_length=1)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for i in range(100):
+    solved = solver.step()
+    print(i, solved)
+
+    if not solved:
+        for fb in solver.get_fibers():
+            for f in fb:
+                p = f.points
+                ax.plot(p[:, 0], p[:, 1], p[:, 2])
+    else:
+        break
+
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+    ax.set_zlim(-2, 2)
+    plt.pause(0.1)
+    ax.cla()
