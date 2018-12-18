dd104c7f2a3b7e10fa502ea706a91c5ceab0bed2
fmatuschke <f.matuschke@fz-juelich.de>
2018-12-18 09:24:10 +0100

    EXAMPLE improved model_solver

--- example/model_solver.py
+++ example/model_solver.py
@@ -4,25 +4,36 @@ import fastpli
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# setup fibers
+VOLUME = 10
+NFIBER = 50
+np.random.seed(42)
 
-fiber_bundles = [[
-    fastpli.objects.Fiber([-1, -1, -1, 1, 1, 1], [1, 1]),
-    fastpli.objects.Fiber([1, 1, -1, -1, -1, 1], [1, 1])]]
+# 2d plane with rnd seedpoints for fibers
+planes = np.empty((NFIBER, 2, 3))
+planes[:, :, 0:2] = np.random.uniform(-VOLUME, +VOLUME, (NFIBER, 2, 2))
+planes[:, 0, 2] = -VOLUME
+planes[:, 1, 2] = +VOLUME
 
+fiber_bundles = [[]]
+for points in planes:
+    fiber_bundles[0].append(
+        fastpli.objects.Fiber(points, np.random.uniform(0.5, 1.0, 2)))
+
+# setup solver
 solver = fastpli.model.Solver()
-
 solver.set_fibers(fiber_bundles)
 solver.set_parameter(drag=0, obj_min_radius=10, obj_mean_length=1)
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
+# run solver and plot results
 for i in range(100):
     solved = solver.step()
-    print(i, solved)
 
     if not solved:
-        print("num:", solver.num_objects)
         for fb in solver.get_fibers():
             for f in fb:
                 p = f.points
@@ -30,9 +41,10 @@ for i in range(100):
     else:
         break
 
-    ax.set_xlim(-2, 2)
-    ax.set_ylim(-2, 2)
-    ax.set_zlim(-2, 2)
+    print("step:", i, solver.num_objects)
+    ax.set_xlim(-1.0 * VOLUME, 1.0 * VOLUME)
+    ax.set_ylim(-1.0 * VOLUME, 1.0 * VOLUME)
+    ax.set_zlim(-1.0 * VOLUME, 1.0 * VOLUME)
+    ax.set_aspect('equal')
     plt.pause(0.1)
     ax.cla()
-    print()
