94e1127862576a3ef44a39a7e5b15570519b8b46
fmatuschke <f.matuschke@fz-juelich.de>
2018-12-19 11:03:11 +0100

    TEST split and combine fibers

--- tests/model/solver_test.py
+++ tests/model/solver_test.py
@@ -7,7 +7,7 @@ import fastpli
 class MainTest(unittest.TestCase):
 
     def setUp(self):
-        self.fiber = fastpli.objects.Fiber([0, 0, 0, 1, 1, 1], [1, 2])
+        self.fiber = fastpli.objects.Fiber([0, 0, 0, 0, 0, 1], [1, 2])
         self.fiberbundles = [[self.fiber]]
         self.solver = fastpli.model.Solver()
         self.solver.set_fiber_bundles(self.fiberbundles)
@@ -18,6 +18,23 @@ class MainTest(unittest.TestCase):
         fb = self.solver.get_fiber_bundles()
         self.assertTrue(np.array_equal(self.fiber.points, fb[0][0].points))
         self.assertTrue(np.array_equal(self.fiber.radii, fb[0][0].radii))
+
+    def test_split(self):
+        self.solver.set_parameters(
+            drag=0, obj_min_radius=0, obj_mean_length=0.5)
+        self.solver.step()
+        fb = self.solver.get_fiber_bundles()
+        self.assertTrue(fb[0][0].radii[1] == 1.5)
+
+    def test_combine(self):
+        self.fiber = fastpli.objects.Fiber(
+            [0, 0, 0, 0, 0, 1, 0, 0, 2], [1, 1, 1])
+        self.fiberbundles = [[self.fiber]]
+        self.solver.set_fiber_bundles(self.fiberbundles)
+        self.solver.set_parameters(drag=0, obj_min_radius=0, obj_mean_length=2)
+        self.solver.step()
+        fb = self.solver.get_fiber_bundles()
+        self.assertTrue(fb[0][0].radii.shape[0] == 2)
 
 
 if __name__ == '__main__':
