b86fdbfadaa1d955d42aa99b6bd1a426c0d01513
fmatuschke <f.matuschke@fz-juelich.de>
2018-12-19 10:58:11 +0100

    FIX return of py-Fiber in py-Solver

--- src/fastpli/model/_solver.py
+++ src/fastpli/model/_solver.py
@@ -4,10 +4,13 @@ from ..objects import Fiber
 
 class Solver(_SolverCPP):
 
-    def get_fibers(self):
-        fiber_bundles = _SolverCPP.get_fibers(self)
-        for fb in fiber_bundles:
-            for f in fb:
-                f = Fiber(f.points, f.radii)
+    def get_fiber_bundles(self):
+        # casting FiberCPP to Fiber
+        fiber_bundles = _SolverCPP.get_fiber_bundles(self)
+        for i in range(len(fiber_bundles)):
+            for j in range(len(fiber_bundles[i])):
+                fiber_bundles[i][j] = Fiber(
+                    fiber_bundles[i][j].points,
+                    fiber_bundles[i][j].radii)
 
         return fiber_bundles
