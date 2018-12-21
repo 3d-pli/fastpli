c88f27537229c2d75b59187b3759e94e655d8ce8
fmatuschke <f.matuschke@fz-juelich.de>
2018-12-21 11:23:53 +0100

    TEST added multiple for model.Solver

--- tests/model/solver_test.py
+++ tests/model/solver_test.py
@@ -21,7 +21,7 @@ class MainTest(unittest.TestCase):
 
     def test_fiber_bundle_property(self):
         self.solver.parameters = (0, 0, 0)
-        test = self.solver.parameters
+        _ = self.solver.parameters
         self.solver.fiber_bundles = [
             [fastpli.objects.Fiber([0, 0, 0, 0, 0, 2], [1, 3])]]
         test = self.solver.fiber_bundles
@@ -42,6 +42,61 @@ class MainTest(unittest.TestCase):
         self.solver.step()
         fb = self.solver.get_fiber_bundles()
         self.assertTrue(fb[0][0].radii.shape[0] == 2)
+
+        self.solver.set_parameters(
+            drag=0,
+            obj_min_radius=0,
+            obj_mean_length=20)
+        self.solver.step()
+        fb = self.solver.get_fiber_bundles()
+        self.assertTrue(fb[0][0].radii.shape[0] == 2)
+
+    def test_fibers(self):
+        self.solver.set_parameters(drag=0, obj_min_radius=0, obj_mean_length=2)
+
+        fiber_0 = fastpli.objects.Fiber([0, 0, 0, 0, 0, 1], [1, 2])
+        fiber_1 = fastpli.objects.Fiber([0, 0, 0.1, 0, 0, 1.1], [1, 2])
+        self.solver.fiber_bundles = [[fiber_0, fiber_1]]
+        self.solver.step()
+        fbs = self.solver.fiber_bundles
+
+        self.assertFalse(np.array_equal(fiber_0.points, fbs[0][0].points))
+        self.assertTrue(np.array_equal(fiber_0.radii, fbs[0][0].radii))
+        self.assertFalse(np.array_equal(fiber_1.points, fbs[0][1].points))
+        self.assertTrue(np.array_equal(fiber_1.radii, fbs[0][1].radii))
+
+    def test_fiber_bundles(self):
+        self.solver.set_parameters(drag=0, obj_min_radius=0, obj_mean_length=2)
+
+        fiber_0 = fastpli.objects.Fiber([0, 0, 0, 0, 0, 1], [1, 2])
+        fiber_1 = fastpli.objects.Fiber([0, 0, 0.1, 0, 0, 1.1], [1, 2])
+        self.solver.fiber_bundles = [[fiber_0], [fiber_1]]
+        self.solver.step()
+        fbs = self.solver.fiber_bundles
+
+        self.assertFalse(np.array_equal(fiber_0.points, fbs[0][0].points))
+        self.assertTrue(np.array_equal(fiber_0.radii, fbs[0][0].radii))
+        self.assertFalse(np.array_equal(fiber_1.points, fbs[1][0].points))
+        self.assertTrue(np.array_equal(fiber_1.radii, fbs[1][0].radii))
+
+    def test_col_voi(self):
+        self.solver.set_parameters(drag=0, obj_min_radius=0, obj_mean_length=2)
+
+        fiber_0 = fastpli.objects.Fiber([0, 0, 0, 0, 0, 1], [1, 2])
+        fiber_1 = fastpli.objects.Fiber([0, 0, 0.1, 0, 0, 1.1], [1, 2])
+        self.solver.fiber_bundles = [[fiber_0], [fiber_1]]
+
+        self.solver.set_col_voi([-10, -10, -10], [-9, -9, -9])
+        self.solver.step()
+        fbs = self.solver.fiber_bundles
+        self.assertTrue(np.array_equal(fiber_0.points, fbs[0][0].points))
+        self.assertTrue(np.array_equal(fiber_0.radii, fbs[0][0].radii))
+
+        self.solver.set_col_voi([0, 0, 0], [1, 1, 1])
+        self.solver.step()
+        fbs = self.solver.fiber_bundles
+        self.assertFalse(np.array_equal(fiber_0.points, fbs[0][0].points))
+        self.assertTrue(np.array_equal(fiber_0.radii, fbs[0][0].radii))
 
 
 if __name__ == '__main__':
