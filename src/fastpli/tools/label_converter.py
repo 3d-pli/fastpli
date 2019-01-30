952bd397c974a45620fe3512d3e850d4cd661c9a
fmatuschke <f.matuschke@fz-juelich.de>
2019-01-30 10:28:41 +0100

    RFC tests for new Dimensions, FORMAT code

--- src/fastpli/tools/label_converter.py
+++ src/fastpli/tools/label_converter.py
@@ -2,15 +2,17 @@ import numpy as np
 import h5py
 from tqdm import tqdm
 
+
 def label_to_txt(data, file_name, replace=[]):
     print(data.shape)
 
     print("writing txt")
     with open(file_name, 'w') as file:
-        file.write( str(data.shape[0]) + " " + str(data.shape[1]) + " " + str(data.shape[2]) + " 2\n")
+        file.write(str(data.shape[0]) + " " + str(
+            data.shape[1]) + " " + str(data.shape[2]) + " 2\n")
 
         # order has to be z,y,x in txt file
-        data = np.transpose(data, (2,1,0))
+        data = np.transpose(data, (2, 1, 0))
         for mat in tqdm(data):
             if replace:
                 mat_cp = mat.copy()
@@ -19,6 +21,7 @@ def label_to_txt(data, file_name, replace=[]):
 
             np.savetxt(file, mat, delimiter='\t', fmt='%i')
             file.write('\n')
+
 
 def h5_to_txt(file_in, dset_name, file_out, replace=[]):
     # TODO: chunking in case of mpi h5
