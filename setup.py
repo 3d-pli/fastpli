74e8b34e034ae10c62c1bea8aec5fd3ea7f636b1
fmatuschke <f.matuschke@fz-juelich.de>
2019-03-08 11:19:41 +0100

    UPDATE version

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='fastpli',
-    version='0.9',
+    version='0.10',
     description='Fiber Architecture Simulation Toolbox for PLI',
     long_description='',
     author='Felix Matuschke',
