491c33d3f3f4522e732794c91e72910c005d1785
fmatuschke <f.matuschke@fz-juelich.de>
2019-03-15 14:28:23 +0100

    ADD binary converter

--- src/fastpli/tools/label_converter.py
+++ src/fastpli/tools/label_converter.py
@@ -4,13 +4,14 @@ from tqdm import tqdm
 
 
 def label_to_txt(data, file_name, replace=[]):
-    print(data.shape)
+
+    gray_level = max(data.flatten()) + 1
 
     print("writing txt")
     with open(file_name, 'w') as file:
         file.write(
             str(data.shape[0]) + " " + str(data.shape[1]) + " " +
-            str(data.shape[2]) + " 2\n")
+            str(data.shape[2]) + " " + str(gray_level) + "\n")
 
         # order has to be z,y,x in txt file
         data = np.transpose(data, (2, 1, 0))
@@ -24,6 +25,32 @@ def label_to_txt(data, file_name, replace=[]):
             file.write('\n')
 
 
+def label_to_binary(data, file_name, replace=[]):
+
+    data = np.array(data, dtype=np.int8)
+    gray_level = max(data.flatten()) + 1
+
+    print("writing binary header")
+    with open(file_name + '.header.txt', 'w') as file:
+        file.write(
+            str(data.shape[0]) + " " + str(data.shape[1]) + " " +
+            str(data.shape[2]) + " " + str(gray_level) + "\n")
+        file.write(file_name + ".binary\n")
+
+    print("writing binary")
+    # order has to be z,y,x in txt file
+    data = np.transpose(data, (2, 1, 0))
+
+    with open(file_name + '.binary', 'wb') as file:
+        for mat in tqdm(data):
+            if replace:
+                mat_cp = mat.copy()
+                for (key, val) in replace:
+                    mat[mat_cp == key] = val
+
+            mat.tofile(file)
+
+
 def h5_to_txt(file_in, dset_name, file_out, replace=[]):
     # TODO: chunking in case of mpi h5
     with h5py.File(file_in, 'r') as h5f:
@@ -31,3 +58,9 @@ def h5_to_txt(file_in, dset_name, file_out, replace=[]):
         data = h5f[dset_name][:]
         label_to_txt(data, file_out, replace)
         # label_to_txt_old(data, file_out, replace)
+
+
+if __name__ == "__main__":
+    data = np.random.randint(5, size=(250, 250, 250))
+    label_to_txt(data, "test.txt")
+    label_to_binary(data, "test")
