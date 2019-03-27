8c7044729b30e7568cfa200c15226493abd44afc
fmatuschke <f.matuschke@fz-juelich.de>
2019-03-27 11:01:06 +0100

    FIX binary header

--- src/fastpli/tools/label_converter.py
+++ src/fastpli/tools/label_converter.py
@@ -3,10 +3,7 @@ import h5py
 from tqdm import tqdm
 
 
-def label_to_txt(data, file_name, replace=[]):
-
-    gray_level = max(data.flatten()) + 1
-
+def label_to_txt(data, file_name, gray_level, replace=[]):
     print("writing txt")
     with open(file_name, 'w') as file:
         file.write(
@@ -25,17 +22,15 @@ def label_to_txt(data, file_name, replace=[]):
             file.write('\n')
 
 
-def label_to_binary(data, file_name, replace=[]):
-
+def label_to_binary(data, file_name, gray_level, replace=[]):
     data = np.array(data, dtype=np.int8)
-    gray_level = max(data.flatten()) + 1
-
     print("writing binary header")
     with open(file_name + '.header.txt', 'w') as file:
         file.write(
-            str(data.shape[0]) + " " + str(data.shape[1]) + " " +
-            str(data.shape[2]) + " " + str(gray_level) + "\n")
-        file.write(file_name + ".binary\n")
+            '{:12d}'.format(data.shape[0]) + '{:12d}'.format(data.shape[1]) +
+            '{:12d}'.format(data.shape[2]) + '{:12d}'.format(gray_level) +
+            ", ImageLx,ImageLy,ImageLz,ImageGray\n")
+        file.write(file_name + ".binary , name of file in binary format\n")
 
     print("writing binary")
     # order has to be z,y,x in txt file
@@ -51,16 +46,30 @@ def label_to_binary(data, file_name, replace=[]):
             mat.tofile(file)
 
 
-def h5_to_txt(file_in, dset_name, file_out, replace=[]):
+def h5_to_txt(file_in, dset_name, file_out, gray_level, replace=[]):
     # TODO: chunking in case of mpi h5
     with h5py.File(file_in, 'r') as h5f:
         print("loading data")
         data = h5f[dset_name][:]
-        label_to_txt(data, file_out, replace)
-        # label_to_txt_old(data, file_out, replace)
+        label_to_txt(data, file_out, gray_level, replace)
+
+
+def h5_to_binary(file_in, dset_name, file_out, gray_level, replace=[]):
+    # TODO: chunking in case of mpi h5
+    with h5py.File(file_in, 'r') as h5f:
+        print("loading data")
+        data = h5f[dset_name][:]
+        label_to_binary(data, file_out, gray_level, replace)
 
 
 if __name__ == "__main__":
     data = np.random.randint(5, size=(250, 250, 250))
-    label_to_txt(data, "test.txt")
-    label_to_binary(data, "test")
+    label_to_txt(data, "test.txt", 4)
+    label_to_binary(data, "test", 4)
+
+    # h5_to_binary(
+    #     "fibers.gzip.h5",
+    #     "tissue",
+    #     "fibers.txt",
+    #     2,
+    #     replace=[(1, 2), (2, 1), (3, 0), (4, 1), (5, 2), (6, 2)])
