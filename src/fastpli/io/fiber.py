1aa7592cad98bcbda24565dc89c4253420535b56
fmatuschke <f.matuschke@fz-juelich.de>
2019-04-02 10:48:24 +0200

    FIX io.fiber

--- src/fastpli/io/fiber.py
+++ src/fastpli/io/fiber.py
@@ -9,17 +9,21 @@ def read(file_name):
     if ext == '.dat' or ext == '.txt':
         with open(file_name, 'r') as f:
             fiber = []
+            flag_fiber_bundle_end = False
             for line in f:
                 if line.strip():
+                    if flag_fiber_bundle_end:
+                        fiber_bundles.append([])
+                        flag_fiber_bundle_end = False
+
                     numbers = list(map(float, line.split()))
                     fiber.append(numbers[0:4])
                 if not line.strip():
                     if fiber:
                         fiber_bundles[-1].append(np.array(fiber))
-                        flag_fiber_end = True
+                        fiber = []
                     else:  # new bundle with double empty line
-                        fiber_bundles.append([])
-                    fiber = []
+                        flag_fiber_bundle_end = True
     else:
         raise TypeError(ext + ' is not implemented yet')
 
@@ -31,7 +35,7 @@ def save(file_name, fiber_bundles):
 
     if ext == '.dat' or ext == '.txt':
         with open(file_name, 'w') as file:
-            for fiber_bundle in fiber_bundles:
+            for fb, fiber_bundle in enumerate(fiber_bundles):
                 for fiber in fiber_bundle:
                     pos, radii = fiber.data
                     for i in range(len(pos)):
@@ -39,7 +43,8 @@ def save(file_name, fiber_bundles):
                             str(pos[i, 0]) + " " + str(pos[i, 1]) + " " +
                             str(pos[i, 2]) + " " + str(radii[i]) + "\n")
                     file.write("\n")
-                file.write("\n")
+                if fb != len(fiber_bundles) - 1:
+                    file.write("\n")
 
     else:
         raise TypeError(ext + ' is not implemented yet')
