92d25123d66f1ba867251757583c5d7ef84a1e93
fmatuschke <f.matuschke@fz-juelich.de>
2019-08-08 13:22:47 +0200

    ADD sandbox.fill.bundle

--- example/model_solver.py
+++ example/model_solver.py
@@ -1,48 +1,42 @@
-import fastpli
-from fastpli.model import bundle_generator
+import fastpli.model.solver
+import fastpli.model.sandbox
 import fastpli.io
 
 import numpy as np
-import time
-import os
+import os, sys
 
-FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+FILE_NAME = os.path.abspath(__file__)
+FILE_PATH = os.path.dirname(FILE_NAME)
+FILE_BASE = os.path.basename(FILE_NAME)
+
 np.random.seed(42)
-''' create fiber_bundle(s)
-'''
-# create fiber_bundle trajectory
+### create fiber_bundle(s) ###
 fiber_bundle_trj = []
 for phi in range(0, 91, 5):
     fiber_bundle_trj.append(
         [200 * np.sin(np.deg2rad(phi)), 200 * np.cos(np.deg2rad(phi)), 0, 1])
-fiber_bundles = [[]]
-fiber_bundles[-1].append(np.array(fiber_bundle_trj))
+fiber_bundle_trj = np.array(fiber_bundle_trj)
 
-# fill fiber_bundle with fibers from a 2d population
-filled_fiber_bundles = [[]]
-population = bundle_generator.populate_circle(64, 1.5 * 5)
-for fb in fiber_bundles:
-    for fiber_obj in fb:
-        fibers = bundle_generator.populate_object(fiber_obj, population, 5)
-        for f in fibers:
-            filled_fiber_bundles[-1].append(f)
+population = fastpli.model.sandbox.fill.circle(20, 5)
 
-fastpli.io.fiber.save('/tmp/fastpli.example.model_solver.dat', fiber_bundles)
-''' setup solver
-'''
-solver = fastpli.model.Solver()
-solver.fiber_bundles = filled_fiber_bundles
+fiber_radii = np.random.uniform(2.0, 5.0, population.shape[0])
+fiber_bundle = fastpli.model.sandbox.fill.bundle(fiber_bundle_trj, population,
+                                                 fiber_radii)
+
+fastpli.io.fiber.save('/tmp/fastpli.example.' + FILE_BASE + '.dat',
+                      [fiber_bundle])
+
+### setup solver ###
+solver = fastpli.model.solver.Solver()
+solver.fiber_bundles = [fiber_bundle]
 solver.drag = 0
 solver.obj_min_radius = 0
 solver.obj_mean_length = 15
-solver.omp_num_threads = 8
+solver.omp_num_threads = 1
 
-solved_fbs = solver.fiber_bundles
-fastpli.io.fiber.save('/tmp/fastpli.example.model_solver_.dat', solved_fbs)
-''' run solver
-'''
+### run solver ###
+solver.draw_scene()
 for i in range(1000):
-
     solved = solver.step()
 
     if i % 10 == 0:
@@ -53,3 +47,6 @@ for i in range(1000):
         print("step:", i, solver.num_obj, solver.num_col_obj)
         solver.draw_scene()
         break
+
+fastpli.io.fiber.save('/tmp/fastpli.example.' + FILE_BASE + '.solved.dat',
+                      solver.fiber_bundles)
--- src/fastpli/model/sandbox/fill.py
+++ src/fastpli/model/sandbox/fill.py
@@ -1,4 +1,5 @@
 import numpy as np
+from ...tools.rotation import a_on_b
 
 
 def rectangle(a, b, spacing, mode='center'):
@@ -21,7 +22,7 @@ def rectangle(a, b, spacing, mode='center'):
                         spacing / 2:2 * dy].reshape(2, -1)
 
     points = np.concatenate((points_0, points_1), axis=1).T
-    points = np.concatenate((points, np.zeros((points.shape[0], 1))), axis=1)
+    # points = np.concatenate((points, np.zeros((points.shape[0], 1))), axis=1)
 
     return points
 
@@ -40,6 +41,71 @@ def circle(radius, spacing):
     dr2 = points[0, :]**2 + points[1, :]**2
 
     points = points[:, dr2 <= radius**2].T
-    points = np.concatenate((points, np.zeros((points.shape[0], 1))), axis=1)
+    # points = np.concatenate((points, np.zeros((points.shape[0], 1))), axis=1)
 
     return points
+
+
+def bundle(traj, seeds, fiber_radius):
+    '''
+    traj: fiber_bundle trajectory: (nx3 or nx4)-array
+    object: fiber seed on a 2d plane: (nx2 or nx3)-array
+    fiber_radius: (int, float, (nx1)-array)
+    '''
+
+    traj = np.array(traj, dtype=float, copy=False)
+    seeds = np.atleast_2d(np.array(seeds, dtype=float, copy=True))
+    print(seeds.shape)
+
+    if len(traj.shape) != 2 or not (traj.shape[1] != 3 or traj.shape[1] != 4):
+        raise TypeError('traj format: (nx3)-array or (nx4)-array')
+
+    if traj.shape[1] != 4:
+        traj = np.append(traj, np.ones(traj.shape[0]), axis=1)
+
+    if traj.shape[0] < 2:
+        raise TypeError('traj size is to small')
+
+    if len(seeds.shape) != 2 or not (seeds.shape[1] != 2 or
+                                     seeds.shape[1] != 3):
+        raise TypeError('seeds format: (nx2)-array or (nx3)-array')
+
+    if seeds.shape[1] == 2:
+        seeds = np.append(seeds, np.zeros((seeds.shape[0], 1)), axis=1)
+
+    if not isinstance(fiber_radius, (int, float, np.ndarray)):
+        raise TypeError('fiber_radius format: (int, float, np.ndarray)')
+
+    if isinstance(fiber_radius, (int, float)):
+        fiber_radius = np.ones(seeds.shape[0]) * fiber_radius
+
+    if len(fiber_radius.shape) != 1 or fiber_radius.size != seeds.shape[0]:
+        raise TypeError('fiber_radius must have the same length as seeds')
+
+    fiber_radius = np.array(fiber_radius, dtype=float, copy=False)
+    fiber_bundle = [np.empty([traj.shape[0], 4]) for i in range(seeds.shape[0])]
+    tangent_old = np.array([0, 0, 1], float)
+
+    for i in range(0, traj.shape[0]):
+        if i == 0:
+            tangent_new = 0.5 * (traj[i + 1, :3] - traj[i, :3])
+        elif i == traj.shape[0] - 1:
+            tangent_new = 0.5 * (traj[i, :3] - traj[i - 1, :3])
+        else:
+            tangent_new = 0.5 * (traj[i + 1, :3] - traj[i - 1, :3])
+
+        if np.all(tangent_new == 0):
+            raise TypeError('same point:', i)
+
+        tangent_new = tangent_new / np.linalg.norm(tangent_new)
+        R = a_on_b(tangent_old, tangent_new)
+
+        for j in range(0, seeds.shape[0]):
+            seeds[j, :] = np.dot(R, seeds[j, :])
+            fiber_bundle[j][i, :] = np.append(
+                seeds[j, :] * traj[i, -1] + traj[i, :3],
+                fiber_radius[j]).copy()
+
+        tangent_old = tangent_new.copy()
+
+    return fiber_bundle
