b1266014d66dc24eb583b0161f36fa4e523ee412
fmatuschke <f.matuschke@fz-juelich.de>
2019-09-09 13:58:57 +0200

    ADD dtype to epa

--- src/fastpli/analysis/epa.py
+++ src/fastpli/analysis/epa.py
@@ -6,17 +6,17 @@ def epa(data):
     data = np.array(data, copy=False)
 
     n = data.shape[-1]
-    rho_2 = 2 * np.linspace(0, np.pi, n, False)
+    rho_2 = 2 * np.linspace(0, np.pi, n, False, dtype=data.dtype)
 
-    a0 = np.sum(data, 2) / n
-    a1 = 2 * np.sum(data * np.sin(rho_2), 2) / n
-    b1 = 2 * np.sum(data * np.cos(rho_2), 2) / n
+    a0 = np.sum(data, -1) / n
+    a1 = 2 * np.sum(data * np.sin(rho_2), -1) / n
+    b1 = 2 * np.sum(data * np.cos(rho_2), -1) / n
 
     t = 2 * a0
     d = 0.5 * np.arctan2(-b1, a1) + np.pi
     r = np.sqrt(a1 * a1 + b1 * b1) / (a0 + 1e-16)
 
-    d.flatten()[d.flatten() > np.pi] -= np.pi
+    d[d > np.pi] -= np.pi
 
     return t, d, r
 
@@ -26,13 +26,13 @@ def direction(data):
     data = np.array(data, copy=False)
 
     n = data.shape[-1]
-    rho_2 = 2 * np.linspace(0, np.pi, n, False)
+    rho_2 = 2 * np.linspace(0, np.pi, n, False, dtype=data.dtype)
 
     a1 = 2 * np.sum(data * np.sin(rho_2), 2) / n
     b1 = 2 * np.sum(data * np.cos(rho_2), 2) / n
 
     d = 0.5 * np.arctan2(-b1, a1) + np.pi
 
-    d.flatten()[d.flatten() > np.pi] -= np.pi
+    d[d > np.pi] -= np.pi
 
     return d
