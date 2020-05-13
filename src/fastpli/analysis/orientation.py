a8e04d43faa8dfbff8e5c5592f49c8e646f91873
fmatuschke <f.matuschke@fz-juelich.de>
2020-05-13 09:10:27 +0200

    ADD analysis.orientation.remap_orientation, fiber_bundles & histogram

--- src/fastpli/analysis/orientation.py
+++ src/fastpli/analysis/orientation.py
@@ -5,10 +5,16 @@ Analyse Methods fiber_bundles orientations
 
 import numpy as np
 from numba import njit
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
 @njit(cache=True)
 def _remap_orientation(phi, theta):
+
+    print(phi.shape)
+    print(theta.shape)
+
     phi = phi % (2 * np.pi)
     theta = theta % np.pi
 
@@ -17,21 +23,36 @@ def _remap_orientation(phi, theta):
     phi[theta < 0] += np.pi
     theta = np.abs(theta)
 
-    phi[theta > .5 * np.pi] += np.pi
-    theta[theta > .5 * np.pi] = np.pi - theta[theta >= .5 * np.pi]
+    mask = theta > .5 * np.pi
+    phi[mask] += np.pi
+    theta[mask] = np.pi - theta[mask]
 
     phi = phi % (2 * np.pi)
 
-    if np.any(phi < 0) or np.any(phi >= 2 * np.pi) or np.any(
-            theta < 0) or np.any(theta > 0.5 * np.pi):
-        raise ValueError
+    # if np.any(phi < 0) or np.any(phi >= 2 * np.pi) or np.any(
+    #         theta < 0) or np.any(theta > 0.5 * np.pi):
+    #     raise ValueError
 
     return phi, theta
 
 
-def fiber_statistic(fiber_bundles,
-                    plot='scatter',
-                    rticks=[10, 20, 30, 40, 50, 60, 70, 80, 90]):
+def remap_orientation(phi, theta):
+    phi = np.array(phi, copy=False)
+    theta = np.array(theta, copy=False)
+    shape = phi.shape
+
+    phi.shape = (-1)
+    theta.shape = (-1)
+
+    _remap_orientation(phi, theta)
+
+    phi.shape = shape
+    theta.shape = shape
+
+    return phi, theta
+
+
+def fiber_bundles(fiber_bundles):
     """
     Calculates the orientation of all fiber segments and plots the result.
 
@@ -39,10 +60,6 @@ def fiber_statistic(fiber_bundles,
     ----------
     fiber_bundles : [[(nx4)-array]]
         fiber_bundle object
-    plot : str 
-        "scatter" or "histogram" plot
-    rticks : list
-        list of radial ticks
 
     Returns
     -------
@@ -50,17 +67,10 @@ def fiber_statistic(fiber_bundles,
         arrays of spherical coordinates phi and theta for all fiber segments
     """
 
-    if plot:
-        import matplotlib.pyplot as plt
-        import matplotlib.cm as cm
-
     gphi = []
     gtheta = []
 
-    fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
-    colors = cm.viridis(np.linspace(0, 1, len(fiber_bundles)))
-
-    for fb, color in zip(fiber_bundles, colors):
+    for fb in fiber_bundles:
         for f in fb:
             if f.shape[0] <= 1:
                 continue
@@ -71,33 +81,54 @@ def fiber_statistic(fiber_bundles,
             gphi.extend(phi)
             gtheta.extend(theta)
 
-            if plot == 'scatter':
-                phi, theta = _remap_orientation(phi, theta)
-                ax.scatter(phi, np.rad2deg(theta), color=color)
-
-    gphi, gtheta = _remap_orientation(np.array(gphi), np.array(gtheta))
-
-    if plot == 'histogram':
-        abins = np.linspace(0, 2 * np.pi, 100)
-        rbins = np.linspace(0, 90, 50)
-        # rbins = np.linspace(0, 0.5 * np.pi, 50)
-
-        #calculate histogram
-        hist, _, _ = np.histogram2d(gphi,
-                                    np.rad2deg(gtheta),
-                                    bins=(abins, rbins))
-        A, R = np.meshgrid(abins, rbins)
-
-        pc = ax.pcolormesh(A, R, hist.T, cmap="viridis")
-        fig.colorbar(pc)
-
-    ax.set_rmax(90)
-    ax.set_rticks(rticks)
-    ax.set_rlabel_position(22.5)
-    ax.set_yticklabels([])
-    ax.grid(True)
-
-    ax.set_title("", va='bottom')
-    plt.show()
+    gphi, gtheta = remap_orientation(gphi, gtheta)
 
     return gphi, gtheta
+
+
+def histogram(phi, theta, ax, n_angle=100, n_radius=50, cmap="viridis"):
+    """
+    Plot the Orientation angles in a histogram
+
+    Parameters
+    ----------
+    phi : array_like
+        list of azimuthal angles
+    theta : array_like
+        list of polar angles
+    ax : Axes object
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    ::
+
+        _, ax = plt.subplots(subplot_kw=dict(projection="polar"))
+        phi = np.random(0,2*np.pi,100)
+        theta = np.random(0,np.pi,100)
+        pc = histogram(phi, theta, ax)
+        plt.colorbar(pc, ax=ax)
+        plt.show()
+    """
+
+    phi, theta = remap_orientation(phi.ravel(), theta.ravel())
+
+    abins = np.linspace(0, 2 * np.pi, n_angle)
+    rbins = np.linspace(0, 90, n_radius)
+
+    #calculate histogram
+    hist, _, _ = np.histogram2d(phi, np.rad2deg(theta), bins=(abins, rbins))
+    A, R = np.meshgrid(abins, rbins)
+
+    pc = ax.pcolormesh(A, R, hist.T, cmap="viridis")
+
+    return pc
+
+    # ax.set_rmax(90)
+    # ax.set_rticks(range(0, 90, 10))
+    # ax.set_rlabel_position(22.5)
+    # ax.set_yticklabels([])
+    # ax.grid(True)
