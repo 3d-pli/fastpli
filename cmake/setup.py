dc9cb750f7c3e292cd549a63430f1c4969a13983
fmatuschke <f.matuschke@fz-juelich.de>
2020-05-18 14:45:37 +0200

    ADD wheel to install_requires

--- cmake/setup.py
+++ cmake/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
         Fibre%20Architecture/Fibre%20Architecture_node.html',
     python_requires='>3.6.0',
     install_requires=[
-        'numpy', 'numba', 'mpi4py', 'scipy', 'h5py', 'matplotlib'
+        'numpy', 'numba', 'mpi4py', 'scipy', 'h5py', 'matplotlib', 'wheel'
     ],
     test_suite='tests',
     zip_safe=False,
