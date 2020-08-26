04d74b862d3c4ea796e57eabbeddd59724c3427f
fmatuschke <f.matuschke@fz-juelich.de>
2020-08-26 17:12:31 +0200

    WIP init scene before setting values

--- src/model/solver/world.hpp
+++ src/model/solver/world.hpp
@@ -54,12 +54,14 @@ class World {
    void DrawScene();
    void ResetView() {
 #if _VIS_LIBRARIES
+      InitScene();
       if (scene_)
          scene_->ResetView();
 #endif
    };
    void ToggleAxis(bool flag) {
 #if _VIS_LIBRARIES
+      InitScene();
       if (scene_)
          scene_->ToggleAxis(flag);
 #endif
@@ -68,6 +70,7 @@ class World {
 
    void ToggleCollisionView(bool flag) {
 #if _VIS_LIBRARIES
+      InitScene();
       if (scene_)
          scene_->ToggleCollisionView(flag);
 #endif
@@ -75,6 +78,7 @@ class World {
    };
    void SetViewCenter(const float x, const float y, const float z) {
 #if _VIS_LIBRARIES
+      InitScene();
       if (scene_)
          scene_->SetViewCenter(x, y, z);
 #endif
@@ -84,6 +88,7 @@ class World {
    };
    void SetViewAngles(const float x, const float y, const float z) {
 #if _VIS_LIBRARIES
+      InitScene();
       if (scene_)
          scene_->SetViewCenter(x, y, z);
 #endif
@@ -93,6 +98,7 @@ class World {
    };
    void SetViewDistance(const float d) {
 #if _VIS_LIBRARIES
+      InitScene();
       if (scene_)
          scene_->SetViewDistance(d);
 #endif
@@ -100,12 +106,14 @@ class World {
    };
    void CloseScene() {
 #if _VIS_LIBRARIES
+      InitScene();
       if (scene_ != nullptr)
          scene_->Close();
 #endif
    };
    void SavePPM(std::string file) {
 #if _VIS_LIBRARIES
+      InitScene();
       if (scene_)
          scene_->SavePPM(file.c_str(), 0, 0);
 #else
