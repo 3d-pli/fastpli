869f592d2c00a3d853544dae010aa5e167d97e2a
fmatuschke <f.matuschke@fz-juelich.de>
2021-02-05 12:31:32 +0100

    FIX if no argument

--- CI/run-build.sh
+++ CI/run-build.sh
@@ -2,7 +2,12 @@
 set -euo pipefail
 
 make clean
-make fastpli $1
+
+if [ $# -eq 0 ]; then
+   make fastpli
+else
+   make fastpli $1
+fi
 
 env-CI/bin/pip3 install --upgrade pip
 env-CI/bin/pip3 install .
